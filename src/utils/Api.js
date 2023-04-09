cf2c8e22779857b4e2643fd026b50e4118c16cb3
Lina Budkova <linabudkova35@gmail.com>
2023-04-09 17:22:14 +0300

    update file

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -6,6 +6,7 @@ export async function getWeather(search) {
   const data = await axios.get(
     `/weather?q=${search}&appid=c3ca235f299a5ac03a9b15b27ae3fee0`
   );
+  console.log(data);
   return data;
 }
 
@@ -13,7 +14,6 @@ export async function getWeatherFiveDays(search) {
   const data = await axios.get(
     `/forecast?q=${search}&appid=c3ca235f299a5ac03a9b15b27ae3fee0`
   );
-  console.log(data);
   return data;
 }
 
